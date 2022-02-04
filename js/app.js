c9ee306c8295b02c8d066fb946ae4137965af1bd
Jim Doyle <jamesdoyle202@gmail.com>
2022-02-03 21:29:53 -0800

    local storage woes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 'use strict';
-
+console.log('hi');
 
 let catalogArray =[]; // collection of collections
 let collection = document.querySelector('form');
-let previewCollection=[]; // array to hold collection so user can view before posting
+let viewCollection = document.getElementById('button');
 let catalogTable = document.getElementById('catalog-body');
 
 let Collection = function(title, condition, price, pressing, size){
@@ -13,7 +13,6 @@ let Collection = function(title, condition, price, pressing, size){
   this.pressing=pressing;
   this.size=size;
   catalogArray.push(this);
-  console.log(this.array);
 };
 
 
@@ -25,45 +24,66 @@ function handleSubmit(event){
   let price= event.target.price.value;
   let pressing= event.target.pressing.value;
   let size= event.target.size.value;
-
+  console.log(catalogArray);
 
   let newCollection=new Collection(title, condition, price,pressing,size);
-  // catalogArray.push(newCollection);
-  catalogArrayLocalStorage();
-  catalogRendering(catalogArray);
+  console.log(catalogArray);
+  catalogArray.push(newCollection);
+  console.log(catalogArray);
 }
 
 function catalogArrayLocalStorage(){
   let stringified = JSON.stringify(catalogArray);
   localStorage.setItem('catalogArrayStorage', stringified);
+  console.log(stringified);
+
 }
 
 function localStorageUnpack(){
   let unpacked = localStorage.getItem('catalogArrayStorage');
-  let parsed = JSON.parse(unpacked);
-  catalogArray= parsed;
-  console.log(parsed);
+  if (unpacked){
+    let parsed = JSON.parse(unpacked);
+    for (let order of parsed){
+      let title = order.title;
+      let condition = order.condition;
+      let price = order.price;
+      let pressing = order.pressing;
+      let size = order.size;
+      let newCollection=new Collection(title, condition, price,pressing,size);
+    }
+  }
+  else{
+    catalogArrayLocalStorage();
+  }
 }
-// localStorageUnpack();
+
+catalogRendering(catalogArray);
 function catalogRendering(array){
   localStorageUnpack();
-  for (let j=0; j < array.length; j++){
-    let tr= document.createElement('tr');
-    catalogTable.appendChild(tr);
-    for (let prop in catalogArray[j]){
-      let td = document.createElement('td');
-      td.textContent = catalogArray[j][prop];
-
-      tr.appendChild(td);
+  if(catalogArray.length>0){
+    for (let j=0; j < array.length; j++){
+      let tr= document.createElement('tr');
+      catalogTable.appendChild(tr);
+      console.log(catalogArray.length);
+      for (let prop in catalogArray[j]){
+        let td = document.createElement('td');
+        td.textContent = catalogArray[j][prop];
+        tr.appendChild(td);
+      }
     }
-
+    console.log(catalogArray.length);
+  }
+  else {
+    console.log('errorrrrrr');
   }
 }
 console.log(catalogArray);
 // catalogRendering(catalogArray);
+console.log(catalogArray.length);
 
 
 
 
 
 collection.addEventListener('submit',handleSubmit);
+// viewCollection.addEventListener('click', catalogRendering);
